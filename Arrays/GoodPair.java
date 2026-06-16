cf7fb358b866b00453ff37188bea88d7139a46d6
Lokesh <gajangilokesh@gmail.com>
2026-06-16 21:08:03 +0530

    This problem is to find no of good pair in an array.I have used HashMap Approach to solve this problem.This is the most optimised solution for this problem.

---
+++ Arrays/GoodPair.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class GoodPair {
+
+    public static int numIdenticalPairs(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        int count = 0;
+
+        for (int num : nums) {
+            count += freq.getOrDefault(num, 0);
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1, 1, 3};
+
+        int result = numIdenticalPairs(nums);
+
+        System.out.println("Number of good pairs: " + result);
+    }
+}
