919029e1fa5aa5079d5cc98a24166e52f7ae8f2f
Lokesh <113814592+Lokesh243@users.noreply.github.com>
2026-08-14 12:50:57 +0530

    Refactor Kadane's Algorithm for maximum subarray sum

--- Arrays/KadaneAlgorithm.java
+++ Arrays/KadaneAlgorithm.java
@@ -3,15 +3,16 @@
 public class KadaneAlgorithm {
 
     public static int maxSubArray(int[] nums) {
-        int currentSum = nums[0];
-        int maxSum = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            currentSum = Math.max(nums[i], currentSum + nums[i]);
-            maxSum = Math.max(maxSum, currentSum);
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            max=Math.max(max,sum);
+            if(sum<0){
+                sum=0;
+            }
         }
-
-        return maxSum;
+        return max;
     }
 
     public static void main(String[] args) {
