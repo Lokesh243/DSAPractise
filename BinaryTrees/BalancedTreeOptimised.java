bd2defc0ea90a360c47b0955dc42043ec0a090d1
Lokesh <113814592+Lokesh243@users.noreply.github.com>
2026-08-20 14:25:22 +0530

    Create BalancedTreeOptimised.java
    Optimised solution to find if a tree is balanced or not just by modifying the max depth of the binary tree code

---
+++ BinaryTrees/BalancedTreeOptimised.java
@@ -0,0 +1,43 @@
+class BalancedTreeOptimised {
+
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    static int height(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        if (left == -1 || right == -1)
+            return -1;
+
+        if (Math.abs(left - right) > 1)
+            return -1;
+
+        return 1 + Math.max(left, right);
+    }
+
+    static boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.println(isBalanced(root));
+    }
+}
